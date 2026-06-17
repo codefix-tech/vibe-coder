f0d1e6c0fae60b20669f58fad4ad9d32771af6f4
Nishant kumar <nishantkrnwd2004@gmail.com>
2026-06-17 14:12:17 +0530

    controller

--- src/main/java/com/minor/project/vibe_coder/controller/AuthController.java
+++ src/main/java/com/minor/project/vibe_coder/controller/AuthController.java
@@ -2,10 +2,15 @@ package com.minor.project.vibe_coder.controller;
 
 
 import com.minor.project.vibe_coder.dto.auth.AuthResponse;
+import com.minor.project.vibe_coder.dto.auth.LoginRequest;
 import com.minor.project.vibe_coder.dto.auth.SignupRequest;
+import com.minor.project.vibe_coder.dto.auth.UserProfileResponse;
 import com.minor.project.vibe_coder.service.AuthService;
+import com.minor.project.vibe_coder.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private AuthService authService;
+    private UserService userService;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> signup(SignupRequest request){
         return ResponseEntity.ok(authService.signup(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(LoginRequest request){
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<UserProfileResponse> grtProfile(){
+        Long userId = 1;
+        return ResponseEntity.ok(userService.getProfile(userId));
     }
 }
--- src/main/java/com/minor/project/vibe_coder/service/AuthService.java
+++ src/main/java/com/minor/project/vibe_coder/service/AuthService.java
@@ -1,10 +1,13 @@
 package com.minor.project.vibe_coder.service;
 
 import com.minor.project.vibe_coder.dto.auth.AuthResponse;
+import com.minor.project.vibe_coder.dto.auth.LoginRequest;
 import com.minor.project.vibe_coder.dto.auth.SignupRequest;
-import org.jspecify.annotations.Nullable;
+
 
 public interface AuthService  {
 
     AuthResponse signup(SignupRequest request);
+
+    AuthResponse login(LoginRequest request);
 }
